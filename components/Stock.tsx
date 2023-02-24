07a48baaa5617d554ada711f2ece8735e6f9d639
Derek <rumbleincybertron@gmail.com>
2023-02-24 14:54:38 +0900

    chg stock props

--- components/Stock.tsx
+++ components/Stock.tsx
@@ -4,23 +4,18 @@ import ReactMarkdown from 'react-markdown';
 
 export type StockProps = {
   id: string;
-  shares: number;
-  stock: {
-    id: string;
-    name: string;
-    ticker: string;
-    index: string;
-    price: number;
-  }
+  name: string;
+  ticker: string;
+  index: string;
+  price: number;
 };
 
-const Stock: React.FC<{ userStock: StockProps }> = ({ userStock }) => {
+const Stock: React.FC<{ stock: StockProps }> = ({ stock }) => {
   return (
-    <div onClick={() => Router.push("/stock/[id]", `/stock/${userStock.id}`)}>
-      <small>{userStock.stock.name}: {userStock.stock.ticker} </small>
-      <small>${userStock.stock.price}</small>
-      <div><small>Shares: {userStock.shares}</small></div>
-      <ReactMarkdown>{userStock.stock.name}</ReactMarkdown>
+    <div onClick={() => Router.push("/stock/[id]", `/stock/${stock.id}`)}>
+      <small>{stock.name}: {stock.ticker} </small>
+      <small>${stock.price}</small>
+      <ReactMarkdown>{stock.name}</ReactMarkdown>
       <style jsx>{`
         div {
           color: inherit;
