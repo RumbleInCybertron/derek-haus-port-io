6221ca6c1c5a9c9b79d1848d95da2f7144da996a
Derek <rumbleincybertron@gmail.com>
2023-04-29 19:35:16 +0900

    refactor main index

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,14 +25,6 @@ type Props = {
 const StockFeed = ({ feed }: Props) => {
   return (
     <Layout>
-      <div>
-        <h1>Stock</h1>
-        {feed.map((stock) => (
-          <div key={stock.id} className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
-            <Stock {...stock} />
-          </div>
-        ))}
-      </div>
       {/* <BarChart />
       <DonutChart />
       <StockData /> */}
