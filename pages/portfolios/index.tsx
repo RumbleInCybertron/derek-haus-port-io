74fd32eda52c647c6f9f6c74b1422ce4588c6159
Derek <rumbleincybertron@gmail.com>
2023-05-02 18:17:44 +0900

    + blank portfolios list page

---
+++ pages/portfolios/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+import { NextPage } from "next";
+
+const Portfolios: NextPage = () => {
+  return(
+    <Layout>
+      <div>
+        Portfolios
+      </div>
+    </Layout>
+  );
+};
+
+export default Portfolios;
