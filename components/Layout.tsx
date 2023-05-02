0409191cc9e686f2481a25aa7b8e6524f9f392f5
Derek <rumbleincybertron@gmail.com>
2023-05-03 01:21:23 +0900

    ch schema

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 interface Props {
@@ -7,7 +8,9 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <div>
-
+      <Head>
+        <title>Port IO</title>
+      </Head>
       <Navbar />
       <div className="container mx-auto">{children}</div>
     </div>
