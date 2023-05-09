15c1b7f8c0f2eb219c9cdef38990332729e8cc60
Derek <rumbleincybertron@gmail.com>
2023-05-09 12:12:20 +0900

    + init user profile page

---
+++ pages/user/profile.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { NextPage } from "next";
+import { GetServerSideProps, GetStaticProps } from "next/types";
+import Router from "next/router";
+import { useSession } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { UserProps } from "@/components/User";
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: String(query.email)
+    },
+  });
+
+  // const user = JSON.stringify(res)
+
+  // if (!user) user = await prisma.user.create({
+  //   data: { 
+  //     name: String(query?.name), 
+  //     email: String(query?.email) 
+  //   },
+  // })
+
+  return {
+    props: {user: JSON.parse(JSON.stringify(user))},
+  }
+};
+
+type Props = {
+  user: UserProps,
+};
+
+const Profile = ({user}: Props) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const { data: session, status } = useSession();
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const body = { name, email };
+      await fetch('/api/post', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      await Router.push('/profile/patch');
+    } catch (err) {
+      console.error(err);
+    }
+    try {
+      const data = await fetch('/user/', {})
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (!session ) return null;
+  // console.log(session);
+  console.log(user);
+  console.log(user.id);
+
+  return (
+    <Layout>
+      <div>
+        <h1>Profile</h1>
+        {user.name}{user.email}
+      </div>
+    </Layout>
+  );
+}
+
+export default Profile;
