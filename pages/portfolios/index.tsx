cc129680cf047868e164ede104b0cc623ed85104
Derek <rumbleincybertron@gmail.com>
2023-05-10 23:23:53 +0900

    update portfolios page

--- pages/portfolios/index.tsx
+++ pages/portfolios/index.tsx
@@ -1,11 +1,27 @@
 import Layout from "@/components/Layout";
-import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { Portfolio } from "@/components/Portfolio";
 
-const Portfolios: NextPage = () => {
-  return(
+const Portfolios = () => {
+  const { data: session } = useSession();
+
+  const user = await prisma.user.findUnique({
+    where: { email: String(session?.user?.email) }
+  });
+  const portfolios = await prisma.portfolio.findMany({
+    where: { userId: user?.id },
+    select: { id: true, name: true }
+  });
+
+  return (
     <Layout>
       <div>
         Portfolios
+        {portfolios.map((portfolio) => (
+          <div key={portfolio.id} className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
+            <Portfolio {...portfolio} />
+          </div>
+        ))}
       </div>
     </Layout>
   );
