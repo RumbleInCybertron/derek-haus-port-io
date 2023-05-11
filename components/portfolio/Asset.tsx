be5edd81e27909f8b661bd28b03ba837693b4cac
Derek <rumbleincybertron@gmail.com>
2023-05-11 20:15:43 +0900

    ch stock asset component

--- components/portfolio/Asset.tsx
+++ components/portfolio/Asset.tsx
@@ -1,15 +1,45 @@
 import { TransactionProps } from "@/components/portfolio/Transaction";
+import Router from "next/router";
+import ReactMarkdown from "react-markdown";
 
 export type StockAssetProps = {
   id: string;
-  transactions: TransactionProps[];
+  name: string;
+  ticker: string;
   shares: number;
   average: number;
+  updatedAt: Date;
+  // transactions: TransactionProps[];
 };
 
 export type CryptoAssetProps = {
   id: string;
-  transactions: TransactionProps[];
+  name: string;
+  ticker: string;
   amount: number;
   average: number;
+  updatedAt: Date;
+  // transactions: TransactionProps[];
 };
+
+export const StockAsset = ({id, name, ticker, shares, average, updatedAt}: StockAssetProps) => {
+  return (
+    <div className="text-inherit p-2" onClick={() => Router.push("/portfolio/asset/[id]", `/portfolio/asset/${id}`)}>
+      <small>{name}: {ticker} </small>
+      <small>Shares: {shares} ${average}</small>
+      <small>Last Updated: {updatedAt.toDateString()}</small>
+      <ReactMarkdown>{name}</ReactMarkdown>
+    </div>
+  )
+}
+
+export const CryptoAsset = ({id, name, ticker, amount, average, updatedAt}: CryptoAssetProps) => {
+  return (
+    <div className="text-inherit p-2" onClick={() => Router.push("/portfolio/asset/[id]", `/portfolio/asset/${id}`)}>
+      <small>{name}: {ticker} </small>
+      <small>Amount: {amount} ${average}</small>
+      <small>Last Updated: {updatedAt.toDateString()}</small>
+      <ReactMarkdown>{name}</ReactMarkdown>
+    </div>
+  )
+}
