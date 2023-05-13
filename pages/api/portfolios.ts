f49f2ce6d9ee0278138e57c7abf74625f59beb1f
Derek <rumbleincybertron@gmail.com>
2023-05-13 16:45:34 +0900

    api GET portfolios of user

---
+++ pages/api/portfolios.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({req});
+  if(req.method === 'GET') {
+    const user = await prisma.user.findUnique({
+      where: { email: String(session?.user?.email) }
+    });
+    const portfolios = await prisma.portfolio.findMany({
+      where: { userId: user?.id },
+      include: {
+        stockAssets: { select: { name: true, shares: true, average: true, id: true } },
+        cryptoAssets: { select: { name: true, amount: true, id: true } }
+      }
+    });
+    console.log("Portfolios: ", portfolios);
+    res.status(200).json(portfolios);
+  };
+};
