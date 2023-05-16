f25d2137886e10213039a960429acc3d81eed0b9
Derek <rumbleincybertron@gmail.com>
2023-05-16 18:06:15 +0900

    fix GET /portfolios of auth user

---
+++ pages/api/portfolios/[email].tsx
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({req});
+  if (req.method === 'GET') {
+    const user = await prisma.user.findUnique({
+      where: { email: String(session?.user?.email) },
+      select: {id: true},
+    });
+    const portfolios = await prisma.portfolio.findMany({
+      where: { userId: user!.id },
+      select: {
+        name: true,
+        stockAssets: { select: { name: true, shares: true, average: true, id: true } },
+        cryptoAssets: { select: { name: true, amount: true, id: true } }
+      }
+    });
+    res.status(200).json(portfolios);
+  };
+};
--- pages/portfolios/index.tsx
+++ pages/portfolios/index.tsx
@@ -1,14 +1,16 @@
 import Layout from "@/components/Layout";
 import { Portfolio, PortfolioProps } from "@/components/Portfolio";
+import { useSession } from "next-auth/react";
 import Router from "next/router";
 import { useEffect, useState } from "react";
 
 export const Portfolios = () => {
+  const {data: session, status} = useSession();
   const [portfolioData, setPortfolioData] = useState([]);
   useEffect(() => {
     const callApi = async () => {
       try {
-        const res = await fetch('/api/portfolios/')
+        const res = await fetch(`/api/portfolios/${session?.user?.email}`)
         setPortfolioData(await res.json());
       } catch (err) {
         console.error(err);
@@ -17,7 +19,7 @@ export const Portfolios = () => {
 
     const result = callApi().catch(console.error);
     console.log(result);
-  }, []);
+  }, [session?.user?.email]);
 
   return (
     <Layout>
@@ -31,8 +33,8 @@ export const Portfolios = () => {
             </div>
           )
           : (
-            portfolioData.map((portfolio: PortfolioProps) => (
-              <div key={portfolio.id} className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
+            portfolioData.map((portfolio: PortfolioProps, i) => (
+              <div key={i} className="bg-purple-800/50 shadow ease-in duration-100 hover:shadow hover:bg-purple-900/50 mb-2 p-3 w-1/3">
                 <Portfolio {...portfolio} />
               </div>
             ))
