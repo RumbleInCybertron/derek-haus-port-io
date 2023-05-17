a444778cb8a1f22eb1c52ea2bfca82edc6dfb104
Derek <rumbleincybertron@gmail.com>
2023-05-17 23:33:47 +0900

    ch api GET portfolio

--- pages/api/portfolio.ts
+++ pages/api/portfolio.ts
@@ -1,20 +1,18 @@
 import { prisma } from "@/lib/prisma";
-import { getSession } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({req});
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
   if(req.method === 'GET') {
-    const user = await prisma.user.findUnique({
-      where: { email: String(session?.user?.email) }
-    });
-    const portfolio = await prisma.portfolio.findUnique({
-      where: { userId: user?.id, id: String(req.query.id) },
-      include: {
-        stockAssets: { select: { name: true, shares: true, average: true, id: true } },
-        cryptoAssets: { select: { name: true, amount: true, id: true } }
+    const port = await prisma.portfolio.findUniqueOrThrow({
+      where: { id: String(id) },
+      select: {
+        id: true,
+        name: true,
+        stockAssets: { select: { id: true, name: true, ticker: true, shares: true, average: true, updatedAt: true } },
+        cryptoAssets: { select: { id: true, name: true, ticker: true, amount: true, average: true, updatedAt: true } },
       }
     });
-    res.status(200).json(portfolio);
+    res.status(200).json(port);
   };
 };
