1b4095c902267d74e07d4b8ec9f739cc98d1e443
Derek <rumbleincybertron@gmail.com>
2023-05-18 18:59:40 +0900

    ch GET port by id

---
+++ pages/api/portfolio/[id].ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  if(req.method === 'GET') {
+    const port = await prisma.portfolio.findUniqueOrThrow({
+      where: { id: String(id) },
+      include: {
+        stockAssets: { select: { id: true, name: true, ticker: true, shares: true, average: true, updatedAt: true }},
+        cryptoAssets: { select: { id: true, name: true, ticker: true, amount: true, average: true, updatedAt: true } },
+      }
+    });
+    res.status(200).json(port);
+  };
+};
